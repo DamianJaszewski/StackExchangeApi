ed460a09bf1b44d1ffcc370bf3f18f9bd1547925
DamianJaszewski <damianjaszewski@gmail.com>
2025-01-24 16:47:31 +0100

    edit TagQueryParams

--- StackExchangeApi/Models/TagQueryParams.cs
+++ StackExchangeApi/Models/TagQueryParams.cs
@@ -5,7 +5,6 @@
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
         public string OrderBy { get; set; } = string.Empty;
-        public string ThenOrderBy { get; set; } = string.Empty;
-        public bool IsDescending { get; set; } = false;
+        public bool IsAscending { get; set; } = true;
     }
 }
